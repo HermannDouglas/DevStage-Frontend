380143bb59cfcbd9afcbd7e92464e48632609c2d
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-02-21 18:49:23 -0500

    Feat: Adicionar Tailwind CSS e fontes personalizadas
    - Integra Tailwind CSS no projeto através do arquivo globals.css
    - Define cores e fontes personalizadas no tema
    - Atualiza o layout para usar as fontes Montserrat e Oxanium
    - Modifica o título da aplicação para "DevStage"
    - Ajusta o estilo do corpo para fundo preto e texto branco

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "./components/button";
+
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+
+      <h1 className="text 4xl font-semibold font-heading">Hello World!</h1>
+
+      <Button text="Hermann" />
+      <Button text="React" />
+      <Button />
     </main>
   );
 }
