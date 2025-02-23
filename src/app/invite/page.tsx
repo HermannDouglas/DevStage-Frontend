86ff4d3b69d286e74c937935776b16e707ef9dba
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-02-22 20:32:08 -0500

    Feat: atualizar InviteLinkInput para receber link como prop
    - Adiciona interface InviteLinkInputProps para tipagem do prop inviteLink
    - Modifica o componente InviteLinkInput para usar o prop inviteLink em vez de um valor fixo
    - Implementa função copyInviteLink para copiar o link de convite para a área de transferência
    - Atualiza a página de convite para passar o link de convite dinâmico para o InviteLinkInput

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -5,6 +5,8 @@ import { Ranking } from './ranking'
 import { Stats } from './stats'
 
 export default function Invite() {
+  const inviteLink = 'http://localhost:3000/invite/5901924080182830'
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -30,7 +32,7 @@ export default function Invite() {
             </p>
           </div>
 
-          <InviteLinkInput />
+          <InviteLinkInput inviteLink={inviteLink} />
 
           <Stats />
         </div>
